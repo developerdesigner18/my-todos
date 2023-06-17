d5747f1f731ce283e6d0045e87cc4cb9bc8ff23b
developerdesigner18 <developer.designer18@gmail.com>
2023-06-17 10:13:11 +0530

    feat: added server

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./libraries/server"; // This does the magic
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "/graphql",
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
